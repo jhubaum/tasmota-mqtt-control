38cf7c97d41f7967d19b1f1d31de1b3a8ae2ea40
Johannes Huwald <johannes.huwald@gmail.com>
2022-08-27 10:02:25 +0200

    Add minimal web app
    Use flask to render web app containing a single button sending a toggle
    command via MQTT. The actual MQTT request is send from a server, since
    the long term idea is not to have a manual control, but to automate the
    control from the server. The web interface is just a dummy setup to
    demonstrate the information flow.

--- server.py
+++ server.py
@@ -1,10 +1,23 @@
 import paho.mqtt.client as mqtt 
-
-import sys
-
 import config
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
 
 client = mqtt.Client("Server Control")
 client.connect(config.MQTT_BROKER) 
 
-client.publish(f"cmnd/{config.TASMOTA_TOPIC}/POWER", None if len(sys.argv) == 1 else sys.argv[1])
+@app.route("/control/<cmd>", methods=["POST"])
+def control_switch(cmd):
+    if cmd in {"on", "off", "toggle"}:
+        client.publish(f"cmnd/{config.TASMOTA_TOPIC}/POWER", cmd)
+        return "Ok", 200
+    return "Unknown command", 401
+
+@app.route("/")
+def home():
+    return render_template('control_panel.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
